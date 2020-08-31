d4416f9bb015247228ce8c808ccdb0b03ec0c9f1
tbdoan <tbdoan@ucsd.edu>
2020-08-31 15:43:56 -0700

    first commit

---
+++ send_sms.js
@@ -0,0 +1,13 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'ACd45d662702b6b78b44834a2a28ab3ace';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+client.messages
+    .create({
+    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+    from: '+15017122661',
+    to: '+15558675310',
+})
+    .then((message) => console.log(message.sid));
