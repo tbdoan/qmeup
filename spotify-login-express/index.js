ec49fff9bb7847574048f69d634226bda5643a74
tbdoan <tbdoan@ucsd.edu>
2020-09-01 11:45:01 -0700

    reformated repo

--- spotify-login-express/index.js
+++ spotify-login-express/index.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 const server = app.listen(8080, () => {
     const host = server.address().address;
     const port = server.address().port;
--- spotify-login-express/index.ts
+++ spotify-login-express/index.ts
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
 const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
